b7ee0126da799003fe3af835aef0df557f23b8ba
Jeremy VONG <gnov.jeremy@gmail.com>
2023-07-05 16:52:46 +0200

    Fix issue with loop not breaking correctly due to nesting of loops, and added some print

--- retrieve_match.py
+++ retrieve_match.py
@@ -8,7 +8,7 @@ image_extension = (".jpg", ".png",".svg","jpeg")
 def get_autographes( csv_source : str ) -> list[str]:
     csv_data = []
     with open(csv_source, newline='') as inputfile:
-        return [row[9] for row in csv.reader(inputfile)]
+        return [row[9] for row in csv.reader(inputfile) if row[9] != ""]
         #csv_data = [row for row in csv.reader(inputfile)]
 
     """BMDV_name_convention = 'BMDV-ms'
@@ -50,33 +50,29 @@ def get_matches( autographes : list[str], images_files : list[str]) -> tuple[int
     # To find matches more efficiently, we remove images already associated
     while i < current_size :
         # Loop though each cote_group (some letter have 2 cotes)
-        
-        for cote_group in cotes.keys() :  
-            for cote in cote_group.split("+") :
-                
-                filename = files[i].lower()
-                for cote_in_name in re.findall(r"(\d+(?:-\d+)*(?:bis+)*(?: bis+)*(?:ter+)*(?: ter+)*)", filename) :
-                    #if "251" in filename :
-                    print(filename+">> "+cote_in_name + " ==? "+ cote )
-                    if cote== cote_in_name.replace(" ","") :
-                        print("FOUND")
-                        #print(filename+">> "+cote_in_name)
+        cotes, count, cotes_availables, files, current_size, i = __compare_match(cotes, count, cotes_availables, files, current_size, i)
+    return count,cotes, cotes_availables
+
+def __compare_match(cotes, count, cotes_availables, files, current_size, i):
+    for cote_group in cotes.keys() :  
+        for cote in cote_group.split("+") :
+            filename = files[i].lower()
+            for cote_in_name in re.findall(r"(\d+(?:-\d+)*(?:bis+)*(?: bis+)*(?:ter+)*(?: ter+)*)", filename) :
+                #print(filename+">> "+cote_in_name + " ==? "+ cote )
+                if cote== cote_in_name.replace(" ","") :
+                    #print(filename+">> "+cote_in_name)
                         
-                        if cote not in cotes_availables :
-                            print("went in")
-                            cotes_availables[cote] = []
-                        cotes_availables[cote].append(files[i])
-                        count+=1
-                        del files[i]
-                        print("del" + files[i])
-                        current_size-=1
-                        i = 0
-                        break # ERROR HERE NEED TO BREAK TILL WHILE
+                    if cote not in cotes_availables :
+                        cotes_availables[cote] = []
+                    cotes_availables[cote].append(files[i])
+                    count+=1
+                    del files[i]
+                    current_size-=1
+                    return cotes, count, cotes_availables, files, current_size, 0
                 
                 #print(filename+" || " + cote_group)
-        else : 
-            i+=1
-    return count,cotes, cotes_availables
+    else : 
+        return cotes, count, cotes_availables, files, current_size, i+1
 
 
 def get_length_list_dict(dictionnary : dict) ->int :
@@ -84,8 +80,6 @@ def get_length_list_dict(dictionnary : dict) ->int :
     for item in dictionnary.values() :
         length+= len(item)
     return length
-
-#print (get_length_list_dict(cotes_availables))
 
 # Dictionnaire
 # cotes : [ code : nom complet de l'autographe ]
@@ -95,33 +89,35 @@ if __name__ == "__main__" :
 
     csv_source = 'Correspondance MDV - Site https __www.correspondancedesbordesvalmore.com - lettres.csv'
     autographes = get_autographes(csv_source)
+    total_found = 0
+    letters_associated = 0
 
     if os.path.exists("result.txt"):
         os.remove("result.txt")
-    for dir in [1] : 
-    #for dir in next(os.walk('Images'))[1] :
-        #1620-49-49ges_dir = "Images/"+dir
-        #path_images_dir = "Images/Ms 1620-3.jpg"
-        path_images_dir = "Images/rest"
+
+    for dir in next(os.walk('Images'))[1] :
+        path_images_dir = "Images/"+dir
 
         images_files = fetch_images(path_images_dir, True)
-        print("Pour le dossier > "+path_images_dir)
-        print("total img > "+ str(len(images_files)) + "| start timer ")
+        print("For the folder  > "+path_images_dir)
+        print("Image count > "+ str(len(images_files)) + "| Timer starting now ")
         start_time =time.time()
-        foundNB,cotes,cotes_associated = get_matches(autographes,images_files)
+        found_matches,cotes,cotes_associated = get_matches(autographes,images_files)
         for i in cotes.items() :
             #print(i)
             pass
+        total_found += found_matches
+        letters_associated += len(cotes_associated)
+        print("Matches found  > " + str(found_matches) + " | Time taken : "+ str(time.time() - start_time))
 
-        print("Correspondance trouvé : > " + str(foundNB) + " | Temps pris : "+ str(time.time() - start_time))
-        print("--------------------")
-
-        with open("result.txt", 'w') as f:
+        with open("result.txt", 'a+') as f:
             for i in cotes_associated.items() :
                 #f.write(str(i)+"\n")
                 f.write(str(i[0])+";"+str(i[1])+"\n")
-                print(str(i[0])+";"+str(i[1]))
-
+                #print(str(i[0])+";"+str(i[1]))
         for i in cotes :
             #print(i)
             pass
+        
+        print("--------------------")
+    print("Found in total "+str(total_found)+" matches of images with letter\nTotalling "+str(letters_associated)+" letters associated")
