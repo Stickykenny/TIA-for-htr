33a58d813e55a568bacfcd0c02c03c0367f86a0c
Jeremy VONG <gnov.jeremy@gmail.com>
2023-07-06 16:42:18 +0200

    Added function to fetch url of file in drive from csv

---
+++ utils_extract.py
@@ -0,0 +1,85 @@
+"""
+Extra Scripts and functions
+"""
+from ast import literal_eval
+import os 
+from shutil import copy
+import csv
+
+def get_column_values( csv_source : str, column : int= 9 ) -> list[str]:
+    """
+    Retrieve into a list all values from the n column
+    """
+    with open(csv_source, newline='') as inputfile:
+        return [row[column] for row in csv.reader(inputfile) if row[column] != ""]
+
+def get_letter_with_n_image(filename : str, n : int=0)-> dict :
+    """
+    Print all letters' cote having only n image
+    if n = 0, this function will simply convert the file into a python dictionnary
+    """
+    letters_fetched = dict()
+    with open(filename, 'r') as f:
+        for line in f.readlines() :
+            line = line.split(":")
+            retrieved_list = literal_eval(line[1])
+            if n != 0 :
+                if (len(retrieved_list) != n) :
+                    continue
+            #print(line[0]+","+str(retrieved_list))
+            letters_fetched[line[0]]= retrieved_list
+    return letters_fetched
+
+def __copy_file(filename, dir_target, file_rename : str = "") :
+    """
+    Function for extracting a file, do nothing if the file doesn't exist
+    Doesn't check if directory exist !
+    """
+    if os.path.exists(filename) :    
+        copy(filename, dir_target)
+        if file_rename != "" :
+            os.rename(dir_target+"os.sep"+filename,dir_target+"os.sep"+file_rename)
+
+def batch_extract_copy(target : dict , output_dir : str="batch_extract") :
+    """
+    Extract all images from the target dictionnary to the output_dir
+    """
+    if not os.path.isdir(output_dir) :
+        os.mkdir(output_dir)
+    for item in target.items() :
+        print(item)
+        for image in [img for img in item[1]] :
+            __copy_file(image, output_dir)
+
+def extract_file_from_drive( csv_source : str, c1 : int,c2 : int=-1, list_to_compare : list[str]= []) :
+    """
+    Extract file at column c1 from google sheet
+    where value in column c2 is present in list_to_compare
+    """
+    list_to_compare = sorted(list_to_compare)
+    result = {}
+    with open(csv_source, newline='') as inputfile:
+        if c1 >= 0 and c2 >= 0 :
+            csv_data = sorted([(row[c1],row[c2]) for row in csv.reader(inputfile) if row[c2] != ""],key=lambda x : x[1])
+            i = j = 0
+            size_csv, size_lst = len(csv_data), len(list_to_compare)
+            while i < size_csv and j < size_lst : 
+                if csv_data[i][1] == list_to_compare[j]:
+                    result[list_to_compare[j]] = csv_data[i][0]
+                    i+=1
+                    j+=1
+                elif csv_data[i][1] < list_to_compare[j]:
+                    i+=1
+                else :
+                    j +=1
+    return result
+
+
+if __name__ == "__main__" :
+
+    letters_fetched = get_letter_with_n_image("result.txt",1)
+    batch_extract_copy(letters_fetched)
+
+    csv_source = 'Correspondance MDV - Site https __www.correspondancedesbordesvalmore.com - lettres.csv'
+    print("extract")
+    print(extract_file_from_drive(csv_source,c1=3,c2=9,list_to_compare=list(letters_fetched.keys())))
