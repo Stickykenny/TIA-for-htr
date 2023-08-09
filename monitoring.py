c27457d6fb62c0d87064115b1b732fc990855224
Jeremy VONG <gnov.jeremy@gmail.com>
2023-08-09 13:44:05 -0200

    Add function to generate an html page to preview text/image alignment

--- monitoring.py
+++ monitoring.py
@@ -69,3 +69,66 @@ def timeit(f):
 
         return result
     return timed
+
+
+def generate_compare_html(source_dir):
+
+    previews_path = "tmp"+os.sep+"cropping_preview"
+    os.makedirs(previews_path, exist_ok=True)
+
+    with open("tmp"+os.sep+"cropping_preview_index.html", 'w', encoding='UTF-8', errors="ignore") as html_index:
+        html_index.write('''<!DOCTYPE html>
+                        <html lang="en">
+                        <head>
+                        <meta charset="UTF-8">
+                        <title>Index of all cropping done next to their text aligned</title>
+                        </head>
+                        <body><ul>''')
+        for dir, subfolders, files in sorted(os.walk(source_dir))[1:]:
+            image_cropping_preview = previews_path + \
+                os.sep+dir.split(os.sep)[-1]+".html"
+            with open(image_cropping_preview, 'w', encoding='UTF-8', errors="ignore") as html:
+
+                html.write('''<!DOCTYPE html>
+                            <html lang="en">
+                            <head>
+                            <meta charset="UTF-8">''')
+                html.write(
+                    '<title>'+os.sep.join(image_cropping_preview.split(os.sep)) + " cropped" + '</title>')
+                html.write('''<script >
+                                    function toggleVisibility(contentId) {
+                                    var content = document.getElementById(contentId);
+                                    content.style.display = (content.style.display == = 'none') ? 'block': 'none';}
+                            </script >
+                            </head >
+                            <body >''')
+                html.write('<br><div style="text-align:center;"><button onclick = "toggleVisibility(\'' +
+                           dir+'\')" > <h1 > '+dir+' </button ></div>')
+                html.write(
+                    '<div style="text-align:center; display:block;" id="'+dir+'">')
+                cropping_count = 0
+                for file in files:
+                    if not file.endswith('.jpg'):
+                        continue
+                    image_path = dir+os.sep+file
+                    html.write('<img style="min-width:500px;max-width:1000px"  src="' + ".."+os.sep + ".."+os.sep +
+                               image_path+'" alt="'+image_path+'" >')
+                    text_filename = ''.join(file.split(".")[:-1])+".gt.txt"
+                    with open(dir+os.sep+text_filename, 'r', encoding='UTF-8', errors="ignore") as text_file:
+                        text_aligned = text_file.readline()
+                        html.write('<p>'+text_aligned+'</p>')
+                    cropping_count += 1
+                html.write('</div></body>')
+
+            href = os.sep.join(image_cropping_preview.split(os.sep)[1:])
+            html_index.write('<li>  <a href="'+href+'">' +
+                             href + ', '+str(cropping_count)+' cropping</a>   </li>')
+            cropping_count = 0
+        html_index.write("</ul></body>")
+
+
+if __name__ == "__main__":
+
+    cropped_dir = "tmp"+os.sep+"cropped_match"
+
+    generate_compare_html(cropped_dir)
