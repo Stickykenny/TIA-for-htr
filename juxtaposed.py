86e1438705e4af43a2c6fbf6e3289bc5a8bab83b
Jeremy VONG <gnov.jeremy@gmail.com>
2023-09-06 11:05:59 +0200

    Add comments and refactoring

--- juxtaposed.py
+++ juxtaposed.py
@@ -1,73 +1,109 @@
 """
-Minor script producing juxtaposition of multiple cropped images and their transcription
+This file will create an image juxtaposing multiple cropped images so that they can in one single image 
+before each cropped image will have an additional image 'mdvocr.png' to help locate the text
+
+Usage : generate_juxtaposed
 """
+
 
 from PIL import Image
 import os
 from PIL import ImageStat, ImageDraw
 
-if __name__ == "__main__":
 
+def generate_juxtaposed():
+
+    # Source of segmented/cropped images
     cropped_dir = "manual_align"
+
     os.makedirs("juxtaposed", exist_ok=True)
 
-    first_img = Image.open("ressources"+os.sep+"mdvocr.png")
+    # Helper image to help google Lens start OCR on this line
+    helper_image = Image.open("ressources"+os.sep+"mdvocr.png")
+    interline_space = 150
 
+    progress_count = 0  # Number of juxtaposed image
+    skip_count = 0  # Number of image skipped because they were already aligned
+    walk_list = list(os.walk(cropped_dir))
 
-    progress_count = 0
-    walk_list =  list(os.walk(cropped_dir))
-
+    # For every set of cropped images
     for dir, subfolders, files in walk_list[1:]:
 
+        # Skip the image if it was manually aligned (json present)
+        json_filename = dir.split(os.sep)[-1].replace(".jpg", ".json")
+        if os.path.exists(cropped_dir+os.sep+json_filename):
+            skip_count += 1
+            continue
+
+        # Images are sorted in alphabetical order
         files = sorted(files)
+
         # Fetch images
         images = [Image.open(dir+os.sep+file)
                   for file in files if file.endswith((".jpg", ".png"))]
 
-        medians = [ImageStat.Stat(img).median for img in images]
-        median_color = medians[len(medians)//2]
         # Skip when no images are found
         if not images:
             continue
 
-        # Define new image shape
+        # Retrieve median color of the images
+        medians = [ImageStat.Stat(img).median for img in images]
+        median_color = medians[len(medians)//2]
+
+        # Define new image shape that will juxtaposed every cropped image
+        # It will take into account the size of the helper image at the start of a line
+        # Run the the code on one example to see a clear example of the wanted result
         sizes = [img.size for img in images]
-        max_width = max([size[0] for size in sizes])+first_img.size[0]+500
-        sum_height = sum([int(size[1]+150) for size in sizes])
-        juxtaposed_image = Image.new('RGB', (max_width, sum_height),  color=tuple(median_color))  # Set default color to white
+
+        # Get the width of the helper image after being resized
+        max_width = max(sizes, key=lambda sizes: sizes[1])
+        first_img_width_max = int(
+            helper_image.size[0]*max_width/helper_image.size[1])
+
+        # Width of the new juxtaposed image ( with the addition of the helper image's width )
+        max_width = max([size[0] for size in sizes]) + first_img_width_max*1.1
+
+        # Height of the new image
+        sum_height = sum([int(size[1]+interline_space) for size in sizes])
+
+        # Create a blank new Image
+        juxtaposed_image = Image.new('RGB', (max_width, sum_height),  color=tuple(
+            median_color))  # Set default color to the median color
 
         # Concatenate all images one under another
-        y_offset = 0
+        y_offset = 0  # y position as in mathematical position
         for img in images:
-            new_width = int(first_img.size[0]*img.size[1]/first_img.size[1])
-            juxtaposed_image.paste(first_img.resize((new_width,img.size[1])), (0, y_offset))
+
+            # Width adjusted for the helper image
+            new_width = int(
+                helper_image.size[0]*img.size[1]/helper_image.size[1])
+
+            # Paste the helper image and the image cropped
+            juxtaposed_image.paste(helper_image.resize(
+                (new_width, img.size[1])), (0, y_offset))
             juxtaposed_image.paste(img, (new_width, y_offset))
 
-            black_separator = Image.new("RGB", (max_width, 150))
-            
-            ## create  rectangleimage
-            #
-            img1 = ImageDraw.Draw(juxtaposed_image)  
-            img1.rectangle([  (0,y_offset-85),(max_width,y_offset-65) ], fill ="#000000")
+            # Create a black rectangle to separate each cropped image
+            img1 = ImageDraw.Draw(juxtaposed_image)
+            img1.rectangle(
+                [(0, y_offset-(interline_space//2)-10), (max_width, y_offset-(interline_space//2)+10)], fill="#000000")
 
-            y_offset += int(img.size[1]+150)
+            # Offset for the new image
+            y_offset += int(img.size[1]+interline_space)
+
             img.close()
 
-        # Save the juxtaposed img into juxtaposed/
-        img_filename = dir.split(os.sep)[-1][:-4]
-        juxtaposed_image.save("juxtaposed"+os.sep +
-                              img_filename+"_juxtaposed.jpg")
-        juxtaposed_image.close()
+        progress_count += 1
+        # Show progress in steps of 10 images juxtaposed
+        if progress_count % 10 == 0:
+            print("juxtaposed "+str(progress_count)+"/"+str(len(walk_list)))
 
-        # Retrieve the transcriptions and append it to a list
-        transcriptions = [dir+os.sep +
-                          file for file in files if file.endswith((".txt"))]
-        transcripts_list = []
-        for transcript in transcriptions:
-            with open(transcript, "r", encoding="UTF-8", errors="ignore") as text_file:
-                transcripts_list.append(text_file.read().replace("\n", ""))
+    print("juxtaposed "+str(progress_count)+"/"+str(len(walk_list)))
+    print("A total of "+str(skip_count) +
+          " images were skipped, because they were already aligned")
 
-        progress_count+=1
-        print("juxtaposed "+str(progress_count)+"/"+str(len(walk_list)))
+    helper_image.close()
 
-    first_img.close()
+
+if __name__ == "__main__":
+    generate_juxtaposed()
