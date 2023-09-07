80b568d2c1b8bade9bc7d3e14c5409158233dee3
Jeremy VONG <gnov.jeremy@gmail.com>
2023-09-07 15:24:49 +0200

    Code refactoring

--- align_google_lens.py
+++ align_google_lens.py
@@ -1,8 +1,36 @@
-# THIS FILE IS STILL BUGGED,  IT IS FOR NOW A TEMPORARY SAVE
 
 import align
 import os
 import shutil
+from add_align import curate_alignments
+
+
+def init_glens_addon():
+    tmp_folder = "glens"+os.sep+"juxta_tmp"
+
+    os.makedirs(tmp_folder, exist_ok=True)
+
+    checker = os.listdir(tmp_folder)
+
+    # If no file is found in glens/juxta_tmp
+    # It will bring out one image for the user to upload to Google Lens
+    if not checker:
+        files = [file for file in os.listdir("juxtaposed") if os.path.isfile(
+            os.path.join("juxtaposed", file))]
+
+        selected_file = os.path.join("juxtaposed", files[0])
+
+        shutil.move(selected_file, tmp_folder)
+        print("Check here : " + tmp_folder)
+        print(" Image file to process in Google Lens : " + files[0])
+        exit()
+
+    # Else
+
+    file_juxta = checker[0]  # filename_juxtaposed.jpg
+    file = file_juxta.replace("_juxtaposed", "")   # filename.jpg
+
+    return file
 
 
 def retrieve_multiline_text() -> str:
@@ -47,63 +75,10 @@ mdvocr consolé des amitier légèren et oubliender de""".replace("\n", " ")
     return unprocessed_ocr
 
 
-if __name__ == "__main__":
+def generate_web_alignment_selector(result, file):
 
-    # Juxtaposition images are in Justaposition
-    # juxta_tmp is the folder the user use to fetch the image to upload to Google Lens
-    # juxta_ocr is where the pairs of text/image are created
-    # juxta_done are the juxtaposition images that are already processed
-
-    tmp_folder = "glens"+os.sep+"juxta_tmp"
-    ocr_result_folder = "glens"+os.sep+"ocr_result"
-    done_folder = "glens"+os.sep+"juxta_done"
     user_folder = "glens"+os.sep+"choose_align"
-
-    os.makedirs(tmp_folder, exist_ok=True)
-    os.makedirs(done_folder, exist_ok=True)
-    os.makedirs(ocr_result_folder, exist_ok=True)
     os.makedirs(user_folder, exist_ok=True)
-
-    checker = os.listdir(tmp_folder)
-
-    # If no file is found in glesn/juxta_tmp
-    # It will bring out one image for the user to upload to Google Lens
-    if not checker:
-        files = [file for file in os.listdir("juxtaposed") if os.path.isfile(
-            os.path.join("juxtaposed", file))]
-
-        selected_file = os.path.join("juxtaposed", files[0])
-
-        shutil.move(selected_file, tmp_folder)
-        print("Check here : " + tmp_folder)
-        print(" Image file to process in Google Lens : " + files[0])
-        exit()
-
-    # Else
-
-    # This instruction will require user action to paste into the dialog box the result of Google Lens OCR
-    unprocessed_ocr = retrieve_multiline_text()
-
-    file_juxta = checker[0]  # filename_juxtaposed.jpg
-    file = file_juxta.replace("_juxtaposed", "")   # filename.jpg
-
-    # Save Google Lens Result
-    os.makedirs(ocr_result_folder, exist_ok=True)
-    with open(ocr_result_folder+os.sep+file.replace(".jpg", ".gt.txt"), "w", encoding="UTF-8", errors="ignore") as ocr_write_file:
-        ocr_write_file.write(unprocessed_ocr)
-
-    # Separate the pattern using "mdvocr"
-    unprocessed_ocr_patterns = unprocessed_ocr.lower().replace("\n", " ").split("mdvocr")
-
-    print(unprocessed_ocr_patterns)
-
-    # Retrieve the original transcriptions
-    with open("tmp"+os.sep+"extract_txt"+os.sep+file[:-4]+'.gt.txt', "r", encoding="UTF-8", errors="ignore") as ref_file:
-        text_reference = ref_file.read()
-
-    print(text_reference)
-    result, used_pattern_index = align.align_patterns(
-        unprocessed_ocr_patterns, text_reference)
 
     # We will follow the alphabetical order for referencing images
     images = ["manual_align"+os.sep+file+os.sep +
@@ -121,7 +96,7 @@ if __name__ == "__main__":
     html_code = '<div id="top" style="text-align: center">'
     index_used = []  # List of all images index index, we then use it to know which image didn't find an alignment
     # For every pattern align a row with image on the left and transcription on the right
-    for i in range(len(used_pattern_index)):
+    for i in range(len(result)):
 
         # Wrapper with an id
         html_code += '<div class="wrapper selected" id="' + \
@@ -136,7 +111,7 @@ if __name__ == "__main__":
 
         # OCR and Transcription aligned on the right
         html_code += '<div class="row right"><p> <input type="text" class="valueSelected" value="' + \
-            str(result[i][2]) + '" size="50" ><br><referenceText class="referenceText">' + \
+            str(result[i][2]) + '" originalValue="'+str(result[i][2])+'" size="50" ><br><referenceText class="referenceText">' + \
             str(result[i][0])+'</referenceText><br></p></div>'
 
         html_code += "</div>"
@@ -152,7 +127,7 @@ if __name__ == "__main__":
                 images[i]+'" alt="' + \
                 images[i]+'" ></div>'
 
-            html_code += '<div class="row right"><p> <input type="text" class="valueSelected" value="' + \
+            html_code += '<div class="row right"><p> <input type="text" class="valueSelected" originalValue="' + \
                 '" size="50" ><br><referenceText class="referenceText">' + \
                 "No correct alignment was found" + \
                 '</referenceText><br>' + '</p></div>'
@@ -164,7 +139,46 @@ if __name__ == "__main__":
     # Fill in the template
     webpage = webpage.replace("CONTENT", html_code)
     webpage = webpage.replace("TOTALCROP", str(len(images)))
+    webpage = webpage.replace("TITLE", file)
 
     # Create the webpage file
     with open(user_folder+os.sep+file.replace('.jpg', '.html'), "w", encoding="UTF-8", errors="ignore") as new_page:
         new_page.write(webpage)
+
+
+def retrieve_patterns(file):
+    ocr_result_folder = "glens"+os.sep+"ocr_result"
+    done_folder = "glens"+os.sep+"juxta_done"
+
+    os.makedirs(done_folder, exist_ok=True)
+    os.makedirs(ocr_result_folder, exist_ok=True)
+
+    # This instruction will require user action to paste into the dialog box the result of Google Lens OCR
+    unprocessed_ocr = retrieve_multiline_text()
+
+    # Save Google Lens Result
+    with open(ocr_result_folder+os.sep+file.replace(".jpg", ".gt.txt"), "w", encoding="UTF-8", errors="ignore") as ocr_write_file:
+        ocr_write_file.write(unprocessed_ocr)
+
+    # Separate the pattern using "mdvocr"
+    unprocessed_ocr_patterns = unprocessed_ocr.lower().replace("\n", " ").split("mdvocr")
+
+    return unprocessed_ocr_patterns
+
+
+if __name__ == "__main__":
+
+    # This instruction can stop the program
+    # It decide the mode the program will be in
+    selected_file = init_glens_addon()
+
+    ocr_patterns = retrieve_patterns(selected_file)
+
+    # Retrieve the original transcriptions
+    with open("tmp"+os.sep+"extract_txt"+os.sep+selected_file[:-4]+'.gt.txt', "r", encoding="UTF-8", errors="ignore") as ref_file:
+        text_reference = ref_file.read()
+
+    result, used_pattern_index = align.align_patterns(
+        ocr_patterns, text_reference)
+
+    generate_web_alignment_selector(result, selected_file)
